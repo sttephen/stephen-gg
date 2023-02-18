c894230aa06a192cdf4c9aaec821d55b7810e498
sttephen <stephenatcollege@Gmail.com>
2023-02-18 03:18:06 +0000

    Added copy tooltip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
     import "../app.css";
     import Navbar from "../components/Navbar.svelte";
+    import {Button, Tooltip} from 'flowbite-svelte';
+
+    let tooltip = "stephen#2067";
+    function tooltipClick() {
+        tooltip = "discord copied";
+        navigator.clipboard.writeText("stephen#2067")
+    }
 </script>
 
 <div class="bg-[#19191c] h-full min-h-[100vh] max-h-[100vh]">
@@ -23,9 +30,8 @@
             <p class="font-bold text-white text-[36px]">Connect with me</p>
             <p class="text-[#A9A9BF] text-[20px]">For business enquires, reach me at <a class="hover:brightness-125 underline decoration-dotted transition duration-110 ease-in-out" href="mailto:hello@stephen.gg">hello@stephen.gg</a>.</p>
             <div class="flex gap-7">
-                <a href="https://discordapp.com/users/676926873669992459" target="_blank" class="hover:brightness-125 transition duration-110 ease-in-out">
-                    <img src="/img/discord.png" class="h-14 hover:brightness-125">
-                </a>
+                <Tooltip class="bg-[#161619]" arrow={true} placement="bottom" triggeredBy="#hover">{tooltip}</Tooltip>
+                <Button on:mouseleave={tooltip="stephen#2067"} on:click={tooltipClick} id="hover" class="cursor-pointer hover:brightness-125 transition duration-110 ease-in-out"><img id="click" src="/img/discord.png" class="h-14 hover:brightness-125"></Button>
                 <a href="https://github.com/sttephen/" target="_blank" class="hover:brightness-125 transition duration-110 ease-in-out">
                     <img src="/img/github.png" class="h-14">
                 </a>
