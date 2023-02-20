82d7a12056c379bb3f3aeb26840d49082efde603
sttephen <stephenatcollege@Gmail.com>
2023-02-20 03:29:59 +0000

    Changed to Flyte

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <div class="bg-[#161619] h-[7vh]">
     <div class="flex text-[#A9A9BF] h-full text-[20px] gap-10 justify-center px-3">
-        <a href="https://virtualventures.io" target="_blank" class="hover:brightness-125 self-center flex gap-2 transition duration-110 ease-in-out">
+        <a href="https://flyte.gg" target="_blank" class="hover:brightness-125 self-center flex gap-2 transition duration-110 ease-in-out">
             <p>Company</p>
             <img class="h-3.5 self-center" src="/img/new-tab.png">
         </a>
